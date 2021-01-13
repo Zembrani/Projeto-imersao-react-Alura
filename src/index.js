bfe42c25dc58294b378ebba4243a3d66d19fc6d0
zembrani <renato.zembrani@hotmail.com>
2021-01-12 23:33:22 -0300

    Adição de Single Page Aplication e do carrossel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Pagina404 = () => (<div>pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact></Route>
+      <Route path="/cadastro/video" component={CadastroVideo} exact></Route>
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact></Route>
+      <Route component={Pagina404}></Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
